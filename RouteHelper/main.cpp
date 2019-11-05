9f15e007ce1c2c7f125000db9fa5695131286fb1
Connection Refused <ConnectionRefusedX@ProtonMail.COM>
2019-11-05 11:07:53 +0800

    Add GetBestInterface function

--- RouteHelper/main.cpp
+++ RouteHelper/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <netioapi.h>
 #include <iphlpapi.h>
 
@@ -38,6 +39,35 @@ MIB_IPFORWARD_ROW2 GetRouteE(const char* address, int netmask, const char* gatew
 	row.Protocol = MIB_IPPROTO_NETMGMT;
 
 	return row;
+}
+
+DLLEXPORT INT GetBestInterfaceE(const char* address, unsigned long* index)
+{
+	SOCKADDR_INET destination = { 0 };
+	MIB_IPFORWARD_ROW2 bestRoute = { 0 };
+	SOCKADDR_INET bestSource = { 0 };
+
+	if (inet_pton(AF_INET, address, &destination.Ipv4.sin_addr))
+	{
+		destination.si_family = AF_INET;
+		destination.Ipv4.sin_family = AF_INET;
+	}
+	else if (inet_pton(AF_INET6, address, &destination.Ipv6.sin6_addr))
+	{
+		destination.si_family = AF_INET6;
+		destination.Ipv6.sin6_family = AF_INET6;
+	}
+	else
+	{
+		return -1;
+	}
+
+	if (NO_ERROR == GetBestRoute2(NULL, NULL, NULL, &destination, NULL, &bestRoute, &bestSource))
+	{
+		return bestRoute.InterfaceIndex;
+	}
+
+	return 0;
 }
 
 DLLEXPORT BOOL CreateRouteE(const char* address, int netmask, const char* gateway, int index, int metric = 0)
