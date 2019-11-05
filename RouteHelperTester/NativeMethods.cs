e00c9dee9a02d2b2a5b5614dcecba94bfd7980f6
Connection Refused <ConnectionRefusedX@ProtonMail.COM>
2019-11-05 11:22:40 +0800

    Add RouteHelperTester project

---
+++ RouteHelperTester/NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RouteHelperTester
+{
+    public static class NativeMethods
+    {
+        [DllImport("..\\..\\..\\..\\RouteHelper\\bin\\Debug\\x64\\RouteHelper.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "GetBestInterfaceE")]
+        public static extern int GetBestInterface(string address);
+
+        [DllImport("..\\..\\..\\..\\RouteHelper\\bin\\Debug\\x64\\RouteHelper.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "CreateRouteE")]
+        public static extern bool CreateRoute(string address, int netmask, string gateway, int index, int metric = 0);
+
+        [DllImport("..\\..\\..\\..\\RouteHelper\\bin\\Debug\\x64\\RouteHelper.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DeleteRouteE")]
+        public static extern bool DeleteRoute(string address, int netmask, string gateway, int index, int metric = 0);
+    }
+}
---
+++ RouteHelperTester/RouteHelperTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace RouteHelperTester
+{
+    public class RouteHelperTester
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(936);
+
+            Console.WriteLine("最佳适配器：{0}", NativeMethods.GetBestInterface("114.114.114.114"));
+            Console.ReadLine();
+        }
+    }
+}
