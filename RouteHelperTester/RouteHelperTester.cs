18827c71a6333f1f68b049b2a74bfbb655faa14f
Connection Refused <ConnectionRefusedX@ProtonMail.COM>
2019-12-04 08:26:43 +0800

    Remove chinese

--- RouteHelperTester/RouteHelperTester.cs
+++ RouteHelperTester/RouteHelperTester.cs
@@ -7,9 +7,7 @@ namespace RouteHelperTester
     {
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.GetEncoding(936);
-
-            Console.WriteLine("最佳适配器：{0}", NativeMethods.GetBestInterface("114.114.114.114"));
+            Console.WriteLine("Best Interface: {0}", NativeMethods.GetBestInterface("114.114.114.114"));
             Console.ReadLine();
         }
     }
