8d24358cfa4e266017107fb1f05d24454304aaa3
Connection Refused <ConnectionRefusedX@ProtonMail.COM>
2019-12-04 08:42:37 +0800

    Remove custom path and format code

--- RouteHelperTester/NativeMethods.cs
+++ RouteHelperTester/NativeMethods.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace RouteHelperTester
 {
     public static class NativeMethods
     {
-        [DllImport("..\\..\\..\\..\\RouteHelper\\bin\\Debug\\x64\\RouteHelper.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "GetBestInterfaceE")]
+        [DllImport("RouteHelper", CallingConvention = CallingConvention.Cdecl, EntryPoint = "GetBestInterfaceE")]
         public static extern int GetBestInterface(string address);
 
-        [DllImport("..\\..\\..\\..\\RouteHelper\\bin\\Debug\\x64\\RouteHelper.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "CreateRouteE")]
+        [DllImport("RouteHelper", CallingConvention = CallingConvention.Cdecl, EntryPoint = "CreateRouteE")]
         public static extern bool CreateRoute(string address, int netmask, string gateway, int index, int metric = 0);
 
-        [DllImport("..\\..\\..\\..\\RouteHelper\\bin\\Debug\\x64\\RouteHelper.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DeleteRouteE")]
+        [DllImport("RouteHelper", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DeleteRouteE")]
         public static extern bool DeleteRoute(string address, int netmask, string gateway, int index, int metric = 0);
     }
 }
